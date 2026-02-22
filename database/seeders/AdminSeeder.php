af7171cd04c39ec11cae00d972a1325997f94304
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2026-02-22 08:26:03 -0500

    Fix AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,16 +15,45 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        // User::factory()->create(
-        // User::create(
-        User::updateOrCreate(
-            ['name'=>'Administrador','email'=>'admin@email.com','email_verified_at'=>now(),'password'=> bcrypt('123123123'),]
-            )->assignRole('admin');
+        // // User::factory()->create(
+        // // User::create(
+        // User::updateOrCreate(
+        //     ['name'=>'Administrador','email'=>'admin@email.com','email_verified_at'=>now(),'password'=> bcrypt('123123123'),]
+        //     )->assignRole('admin');
 
-        // User::create(
-        User::updateOrCreate(
-            ['name'=>'JoseDaniel','email'=>'jose.jdgo97@gmail.com','email_verified_at'=>now(),'password' => bcrypt('123123123'),]
-            )->assignRole('superAdmin');
+        // // User::create(
+        // User::updateOrCreate(
+        //     ['name'=>'JoseDaniel','email'=>'jose.jdgo97@gmail.com','email_verified_at'=>now(),'password' => bcrypt('123123123'),]
+        //     )->assignRole('superAdmin');
+
+        // Asegurar que existan los roles
+        Role::firstOrCreate(['name' => 'admin']);
+        Role::firstOrCreate(['name' => 'superAdmin']);
+
+        // Crear / actualizar Admin
+        $admin = User::updateOrCreate(
+            ['email' => 'admin@email.com'], // condición
+            [
+                'name' => 'Administrador',
+                'password' => bcrypt('123123123'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $admin->syncRoles(['admin']);
+
+        // Crear / actualizar Jose
+        $jose = User::updateOrCreate(
+            ['email' => 'jose.jdgo97@gmail.com'], // condición
+            [
+                'name' => 'JoseDaniel',
+                'password' => bcrypt('123123123'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $jose->syncRoles(['superAdmin']);
+    }
 
     }
 }
