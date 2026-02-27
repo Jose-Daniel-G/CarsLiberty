ec0f6c6d947ccf8c231e2b69c74ff86552652324
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2026-02-27 14:49:31 -0500

    First test

---
+++ database/factories/AgendaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Profesor;
+use App\Models\Cliente;
+use App\Models\Curso;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AgendaFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'start' => $this->faker->dateTimeBetween('now', '+1 week'),
+            'end' => $this->faker->dateTimeBetween('+1 week', '+2 weeks'),
+            'user_id' => User::factory(),
+            'profesor_id' => Profesor::factory(),
+            'cliente_id' => Cliente::factory(),
+            'curso_id' => Curso::factory(),
+        ];
+    }
+}
---
+++ database/factories/AsistenciaFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Asistencia>
+ */
+class AsistenciaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'agenda_id' => \App\Models\Agenda::factory(),
+            'asistio' => $this->faker->boolean(),
+            // otros campos...
+        ];
+    }
+}
