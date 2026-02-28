22faa78a9857377d56d637a1f2019938054b21f1
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2026-02-27 20:21:21 -0500

    fixing tests in branch juan/dev 5

--- database/factories/AgendaFactory.php
+++ database/factories/AgendaFactory.php
@@ -14,12 +14,14 @@ class AgendaFactory extends Factory
     public function definition(): array
     {
         return [
+            'title' => $this->faker->sentence(3),
             'start' => $this->faker->dateTimeBetween('now', '+1 week'),
             'end' => $this->faker->dateTimeBetween('+1 week', '+2 weeks'),
-            'user_id' => User::factory(),
-            'profesor_id' => Profesor::factory(),
-            'cliente_id' => Cliente::factory(),
-            'curso_id' => Curso::factory(),
+            // 'color' => $this->faker->hexColor(), // O safeColorName()
+            'user_id' => \App\Models\User::factory(),
+            'cliente_id' => \App\Models\Cliente::factory(),
+            'profesor_id' => \App\Models\Profesor::factory(),
+            'curso_id' => \App\Models\Curso::factory(),
         ];
     }
 }
